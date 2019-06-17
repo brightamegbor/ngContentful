78c80348354e9e89255d00043917aedabb874d31
Bright Amegbor <brytamegbor@gmail.com>
2019-06-17 11:10:32 +0000

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ContentfulService } from './contentful.service';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { MdToHtmlPipe } from './md-to-html.pipe';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/courses', pathMatch: 'full' },
+  { path: 'courses', component: CourseListComponent },
+  { path: 'course/:id', component: CourseDetailsComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    CourseDetailsComponent,
+    MdToHtmlPipe
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [ContentfulService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
